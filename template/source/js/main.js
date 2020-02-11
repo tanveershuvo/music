2d0673f062b8c3825c7754460a62e12fcce002a1
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-02-11 10:19:00 +0200

    Finished homepage design

--- template/source/js/main.js
+++ template/source/js/main.js
@@ -1,1 +1,15 @@
- 
+//  All code will be rewriten using angular
+// It's just here for test
+
+function showLogin(){
+    document.body.classList.add("show-modal-login");
+}
+function hideLogin(){
+    document.body.classList.remove("show-modal-login");
+}
+function showRegister(){
+    document.body.classList.add("show-modal-register");
+}
+function hideRegister(){
+    document.body.classList.remove("show-modal-register");
+}

64c7f9ea1ebad06e849cb55bef472d887d5ceb91
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-02-21 09:49:38 +0200

    Add home page components
    Added the basic layout components with home route:
    	+ Navbar component
    	+ Footer component
    	+ Home component
    	+ Login component
    	+ Register component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './comps/home/home.component';
+import { LoginComponent } from './comps/auth/login/login.component';
+import { RegisterComponent } from './comps/auth/register/register.component';
+import { NavbarComponent } from './comps/navbar/navbar.component';
+import { FooterComponent } from './comps/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
